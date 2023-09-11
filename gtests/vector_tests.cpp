e54e0d564460115499bfd91980bb9fdf9c5da3e6
Aleksey Aleksandrov <yohoho4721@gmail.com>
2023-09-11 23:35:55 +0300

    Add tests

--- gtests/vector_tests.cpp
+++ gtests/vector_tests.cpp
@@ -3,93 +3,112 @@
 #include <vector>
 
 TEST(vector, is_created) {
-    std::vector<int> list1;
-    std::vector<int> *ptr = &list1;
-    ASSERT_FALSE(ptr == nullptr);
+  std::vector<int> vector1;
+  std::vector<int> *ptr = &vector1;
+  ASSERT_FALSE(ptr == nullptr);
 }
 
 TEST(vector, is_empty) {
-    std::vector<int> list1;
-    std::vector<int> list2 = {};
-    ASSERT_TRUE(list1.empty());
-    ASSERT_TRUE(list2.empty());
+  std::vector<int> vector1;
+  std::vector<int> vector2 = {};
+  ASSERT_TRUE(vector1.empty());
+  ASSERT_TRUE(vector2.empty());
 }
 
 TEST(vector, is_equal_empty) {
-    std::vector<int> list1;
-    std::vector<int> list2 = {};
-    ASSERT_TRUE(list1 == list2);
+  std::vector<int> vector1;
+  std::vector<int> vector2 = {};
+  ASSERT_TRUE(vector1 == vector2);
 }
 
 TEST(vector, is_equal_push_back_insert) {
-    std::vector<int> list1;
-    std::vector<int> list2;
-    list1.push_back(1);
-    list2.insert(list2.begin(), 1);
-    ASSERT_TRUE(list1 == list2);
+  std::vector<int> vector1;
+  std::vector<int> vector2;
+  vector1.push_back(1);
+  vector2.insert(vector2.begin(), 1);
+  ASSERT_TRUE(vector1 == vector2);
 }
 
 TEST(vector, push_back) {
-    std::vector<int> list1;
-    std::vector<int> list2 = {1, 2, 3};
-    list1.push_back(1);
-    list1.push_back(2);
-    list1.push_back(3);
-    ASSERT_TRUE(list1 == list2);
+  std::vector<int> vector1;
+  std::vector<int> vector2 = {1, 2, 3};
+  vector1.push_back(1);
+  vector1.push_back(2);
+  vector1.push_back(3);
+  ASSERT_TRUE(vector1 == vector2);
 }
 
 TEST(vector, push_front) {
-    std::vector<int> list1;
-    std::vector<int> list2 = {1, 2, 3};
-    list1.insert(list1.begin(), 3);
-    list1.insert(list1.begin(), 2);
-    list1.insert(list1.begin(), 1);
-    ASSERT_TRUE(list1 == list2);
+  std::vector<int> vector1;
+  std::vector<int> vector2 = {1, 2, 3};
+  vector1.insert(vector1.begin(), 3);
+  vector1.insert(vector1.begin(), 2);
+  vector1.insert(vector1.begin(), 1);
+  ASSERT_TRUE(vector1 == vector2);
 }
 
 TEST(vector, middle_insert) {
-    std::vector<int> list1 = {1, 3};
-    std::vector<int> list2 = {1, 2, 3};
-    list1.insert(std::next(list1.begin(), 1), 2);
-    ASSERT_TRUE(list1 == list2);
+  std::vector<int> vector1 = {1, 3};
+  std::vector<int> vector2 = {1, 2, 3};
+  vector1.insert(std::next(vector1.begin(), 1), 2);
+  ASSERT_TRUE(vector1 == vector2);
 }
 
 TEST(vector, pop_back) {
-    std::vector<int> list1 = {1, 2};
-    std::vector<int> list2 = {1, 2, 3};
-    list2.pop_back();
-    ASSERT_TRUE(list1 == list2);
+  std::vector<int> vector1 = {1, 2};
+  std::vector<int> vector2 = {1, 2, 3};
+  vector2.pop_back();
+  ASSERT_TRUE(vector1 == vector2);
 }
 
 TEST(vector, erase) {
-    std::vector<int> list1 = {2, 3};
-    std::vector<int> list2 = {1, 2, 3};
-    list2.erase(list2.begin());
-    ASSERT_TRUE(list1 == list2);
+  std::vector<int> vector1 = {2, 3};
+  std::vector<int> vector2 = {1, 2, 3};
+  vector2.erase(vector2.begin());
+  ASSERT_TRUE(vector1 == vector2);
 }
 
 TEST(vector, size) {
-    std::vector<int> list1 = {1, 2, 3};
-    std::vector<int> list2 = {};
-    std::vector<int> list3;
-    ASSERT_TRUE(list1.size() == 3);
-    ASSERT_TRUE(list2.size() == 0);
-    ASSERT_TRUE(list3.size() == 0);
+  std::vector<int> vector1 = {1, 2, 3};
+  std::vector<int> vector2 = {};
+  std::vector<int> vector3;
+  ASSERT_TRUE(vector1.size() == 3);
+  ASSERT_TRUE(vector2.size() == 0);
+  ASSERT_TRUE(vector3.size() == 0);
 }
 
 TEST(vector, clear) {
-    std::vector<int> list1 = {1, 2, 3};
-    std::vector<int> list2 = {};
-    list1.clear();
-    ASSERT_TRUE(list1 == list2);
+  std::vector<int> vector1 = {1, 2, 3};
+  std::vector<int> vector2 = {};
+  vector1.clear();
+  ASSERT_TRUE(vector1 == vector2);
 }
 
 TEST(vector, get_element) {
-    std::vector<int> list1 = {1, 2, 3};
-    auto it = list1.begin();
-    ASSERT_TRUE(*it == 1);
-    std::advance(it, 1);
-    ASSERT_TRUE(*it == 2);
-    std::advance(it, 1);
-    ASSERT_TRUE(*it == 3);
+  std::vector<int> vector1 = {1, 2, 3};
+  auto it = vector1.begin();
+  ASSERT_TRUE(*it == 1);
+  std::advance(it, 1);
+  ASSERT_TRUE(*it == 2);
+  std::advance(it, 1);
+  ASSERT_TRUE(*it == 3);
+}
+
+TEST(vector, copy) {
+  std::vector<int> vector1 = {1, 2, 3};
+  std::vector<int> vector2 = vector1;
+  ASSERT_TRUE(vector1 == vector2);
+}
+
+TEST(vector, move) {
+  std::vector<int> full = {1, 2, 3};
+  std::vector<int> empty;
+
+  std::vector<int> vector1 = {1, 2, 3};
+  std::vector<int> vector2;
+
+  vector2 = std::move(vector1);
+
+  ASSERT_TRUE(vector1 == empty);
+  ASSERT_TRUE(vector2 == full);
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int main(){
-    std::cout << "Empty main" << std::endl;
-    return 0;
+int main() {
+  std::cout << "Empty main" << std::endl;
+  return 0;
 }

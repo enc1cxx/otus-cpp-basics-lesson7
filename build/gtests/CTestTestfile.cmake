1825e6abc263fed3f200af5c82bbdb8d05431d89
Алексей Александров <93279148+enconemusic@users.noreply.github.com>
2023-09-11 23:42:17 +0300

    Delete build directory

--- build/gtests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/User/source/repos/otus-cpp-basics-lesson7/gtests
-# Build directory: C:/Users/User/source/repos/otus-cpp-basics-lesson7/build/gtests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../_deps/googletest-build")
